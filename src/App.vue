7d3c43ba66fd70ff4bcad5a830766c7fc3a9e7b2
samchencode <samuel88835@gmail.com>
2020-03-12 04:37:15 -0400

    added list of social links

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
     <Hero :badges="badges" />
+    <SociaList />
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue';
+import SociaList from '@/components/SociaList.vue';
 import '@/main.css';
 
 export default {
   name: 'App',
-  components: { Hero },
+  components: { Hero, SociaList },
   data: () => ({
     badges: ['Full Stack App Development', 'Machine Learning'],
   }),
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  margin: 0 auto;
+  max-width: 1000px;
+  position: relative;
+}
+</style>
---
+++ src/components/SociaList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="socialist">
+    <a href="#" class="socialist__link">
+      <i class="socialist__icon fas fa-envelope"></i>
+    </a>
+    <a href="#" class="socialist__link">
+      <i class="socialist__icon fab fa-github"></i>
+    </a>
+    <a href="#" class="socialist__link">
+      <i class="socialist__icon fab fa-codepen"></i>
+    </a>
+    <a href="#" class="socialist__link">
+      <i class="socialist__icon fab fa-linkedin"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SociaList',
+};
+</script>
+
+<style scoped>
+.socialist {
+  position: absolute;
+  right: 0;
+  top: var(--spacer);
+  display: flex;
+  flex-direction: column;
+}
+
+.socialist::after {
+  content: '';
+  height: 50vh;
+  margin-top: var(--spacer);
+  width: 0;
+  border-left: 2px solid var(--color-light);
+  align-self: center;
+}
+
+.socialist__link {
+  font-size: 2rem;
+  margin-bottom: .1rem;
+}
+
+.socialist__icon {
+  color: var(--color-light);
+  transition: color ease-out 150ms;
+}
+
+.socialist__icon:hover,
+.socialist__icon:hover {
+  color: var(--color-accent);
+}
+
+@media (max-width: 800px) {
+  .socialist {
+    flex-direction: row;
+  }
+
+  .socialist::after {
+    display: none;
+  }
+
+  .socialist__link {
+    margin: 0 .4rem;
+  }
+}
+</style>
