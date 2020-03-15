94dc81fca74599916b292d9b1585590487b56bf8
samchencode <samuel88835@gmail.com>
2020-03-15 19:23:49 -0400

    created footer layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,70 @@
+<template>
+  <footer class="footer">
+    <GithubStats class="footer__github" />
+    <p class="footer__copyright">
+      Made with
+      <i class="fas fa-heart"></i>
+      By
+      Sam Chen
+    </p>
+  </footer>
+</template>
+
+<script>
+import GithubStats from './GithubStats.vue';
+
+export default {
+  name: 'Footer',
+  components: {
+    GithubStats,
+  },
+};
+</script>
+
+<style scoped>
+.footer {
+  display: grid;
+  grid-template-columns: var(--spacer) repeat(8, 1fr) var(--spacer);
+  grid-template-rows: repeat(2, 1fr);
+  justify-items: left;
+  align-items: start;
+}
+
+.footer::before,
+.footer__github,
+.footer__copyright {
+  grid-column: 2 / -4;
+}
+
+.footer::before,
+.footer__copyright {
+  grid-row: 1;
+}
+
+.footer::before {
+  content: '';
+  width: 100%;
+  border-top: var(--border-width) solid var(--color-white);
+}
+
+.footer__github,
+.footer__copyright {
+  font-size: var(--font-size-6);
+  line-height: .9;
+}
+
+.footer__copyright {
+  align-self: end;
+}
+
+.footer__github {
+  grid-row: 2;
+}
+
+@media screen and (min-width: 800px) {
+  .footer {
+    grid-template-columns: 1fr repeat(8, minmax(100px, 125px)) 1fr;
+  }
+}
+
+</style>
---
+++ src/components/GithubStats.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="github footer__github">
+    <span class="github__stars">Stars</span>
+    <span class="github__forks">Forks</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GithubStats',
+};
+</script>
+
+<style></style>
