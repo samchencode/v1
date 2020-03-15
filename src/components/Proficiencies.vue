9384ea41d0ed6ee2dabb572087abba2576dcd434
samchencode <samuel88835@gmail.com>
2020-03-15 09:27:02 -0400

    text displayed properly on mobile, added hover fx

--- src/components/Proficiencies.vue
+++ src/components/Proficiencies.vue
@@ -7,17 +7,53 @@
       :transform="`rotate(45) translate(${svgContainerWidth * -0.07},0)`"
       overflow="visible"
     >
-      <g>
-        <g v-for="[height, circles] of layers" :key="'layer-' + height">
-          <circle
-            v-for="(c, i) of circles"
-            :key="'circle-' + height + i"
-            :cx="c.x"
-            :cy="c.y"
-            :r="c.r"
-            :fill="scaleColor(height, c)"
-          />
+      <g text-anchor="middle">
+        <g v-for="[height, nodes] of layers" :key="'layer-' + height">
+          <g
+            v-for="n of nodes"
+            :key="'node-' + n.data.id"
+            :transform="`translate(${n.x},${n.y})`"
+          >
+            <circle
+              @mouseenter="handleMouseEnterCircle(n)"
+              @mouseleave="handleMouseLeaveCircle(n)"
+              :r="n.r"
+              :fill="scaleColor(height, n)"
+              :stroke="focusCircle === n.data.id && 'white'"
+            />
+            <text
+              transform="rotate(-45)"
+              v-if="n.height === 0"
+              font-size=".48em"
+              y=".5em"
+            >
+              <tspan
+                v-for="(word, i) of n.data.name.split(/[ \.]/)"
+                :key="i"
+                x="0"
+                :y="i + 'em'"
+                :dy="
+                  i * 0.2 - 0.2 * (n.data.name.split(/[ \.]/).length - 1) + 'em'
+                "
+              >
+                {{ word }}
+              </tspan>
+            </text>
+          </g>
         </g>
+        <path
+          v-for="n of layers.get(1)"
+          :key="'path-' + n.id"
+          :id="'path-' + n.id"
+          :d="generatePath(n)"
+          fill="none"
+          :transform="`translate(${n.x},${n.y}) rotate(${getPathMeta(n).rot})`"
+        />
+        <text v-for="n of layers.get(1)" :key="'text-' + n.data.id">
+          <textPath :href="'#path-' + n.id" startOffset="50%" fill="#eff1ed">
+            {{ n.data.name }}
+          </textPath>
+        </text>
       </g>
     </svg>
   </section>
@@ -33,51 +69,78 @@ export default {
   name: 'Proficiencies',
   data() {
     return {
-      svgContainerWidth: 0,
+      mounted: false,
       container: null,
-      proficiencyData: null,
-      root: null,
-      layers: null,
-      scaleColor: null,
+      svgContainerWidth: -1,
+      svgContainerHeight: -1,
+      focusCircle: null,
     };
   },
   mounted() {
     this.container = this.$refs.container;
-
-    this.loadData();
-    this.createPack();
-    window.addEventListener('resize', this.handleResize);
-
-    this.setScaleColor();
+    this.updateSvgContainerDims();
+    window.addEventListener('resize', this.updateSvgContainerDims);
+    this.mounted = true;
   },
   methods: {
-    loadData() {
-      const stratify = d3
+    generatePath(node) {
+      const { clockwise, radiusPaddingEm } = this.getPathMeta(node);
+      const clockwiseMultiplier = clockwise ? 1 : -1;
+      const radius = node.r + this.fontSize * radiusPaddingEm;
+      const arcGenerator = d3
+        .arc()
+        .startAngle((clockwiseMultiplier * -Math.PI) / 2)
+        .endAngle((clockwiseMultiplier * Math.PI) / 2);
+      return arcGenerator({ outerRadius: radius });
+    },
+    getPathMeta(node) {
+      const pathMeta = {
+        2: { rot: 130, radiusPaddingEm: 0.9, clockwise: false },
+        3: { rot: -50, radiusPaddingEm: 0.2, clockwise: true },
+        4: { rot: 0, radiusPaddingEm: 0.2, clockwise: true },
+        5: { rot: 180, radiusPaddingEm: 0.9, clockwise: false },
+      };
+      return pathMeta[node.data.id];
+    },
+    updateSvgContainerDims() {
+      this.svgContainerWidth = this.container.clientWidth;
+      this.svgContainerHeight = this.container.clientHeight;
+    },
+    handleMouseEnterCircle(node) {
+      if (node.height !== 0) return;
+      this.focusCircle = node.data.id;
+    },
+    handleMouseLeaveCircle(node) {
+      if (node.height !== 0) return;
+      this.focusCircle = null;
+    },
+  },
+  computed: {
+    fontSize() {
+      if (!this.mounted) return -1;
+      return parseFloat(getComputedStyle(this.container).fontSize);
+    },
+    proficiencyData() {
+      return d3
         .stratify()
         .id((d) => d.name)
-        .parentId((d) => d.type);
-
-      this.proficiencyData = stratify(placeholderData)
+        .parentId((d) => d.type)
+        .call(null, placeholderData)
         .sum((d) => (d.children ? d.children.length : 0) + 1)
-        .sort((a, b) => (b.data.category > a.data.category) - 0.5)
+        .sort((a, b) => (b.data.category < a.data.category) - 0.5)
         .sort((a, b) => b.value - a.value);
     },
-    createPack() {
-      this.svgContainerWidth = this.container.clientWidth;
-      const width = this.container.clientWidth * 1.14;
-      const height = this.container.clientHeight;
-
-      this.root = d3
+    root() {
+      return d3
         .pack()
-        .size([width, height])
+        .size([this.svgContainerWidth * 1.14, this.svgContainerHeight])
         .call(null, this.proficiencyData);
-      this.layers = d3.group(this.root.descendants(), (d) => d.height);
     },
-    setScaleColor() {
-      this.scaleColor = colorScale(this.root.descendants());
+    layers() {
+      return d3.group(this.root.descendants(), (d) => d.height);
     },
-    handleResize() {
-      this.createPack();
+    scaleColor() {
+      return colorScale(this.root.descendants());
     },
   },
 };
@@ -86,10 +149,10 @@ export default {
 <style>
 .proficiencies {
   overflow: hidden;
+  display: grid;
 }
 
 .proficiencies__svg {
   width: 100%;
-  height: 100%; /* change if dont want it centered */
 }
 </style>
